7acdd104e4e53832add85879bd43d1b0eb631028
keshav-v2004 <keshavcse.ucs1536@gmail.com>
2025-06-26 18:27:30 +0530

    dependencies added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -49,6 +50,25 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.firebase.auth)
+    implementation(libs.androidx.credentials)
+    implementation(libs.androidx.credentials.play.services.auth)
+    implementation(libs.googleid)
+
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.0")
+    implementation ("androidx.navigation:navigation-compose:2.9.0")
+    implementation ("androidx.compose.material:material-icons-extended:1.7.8")
+
+    implementation("com.google.ai.client.generativeai:generativeai:0.9.0")
+    implementation("com.google.code.gson:gson:2.13.1")
+
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.9.0")
+
+    // DataStore for local preferences
+    implementation("androidx.datastore:datastore-preferences:1.1.7")
+
+    implementation("com.google.android.gms:play-services-auth:21.3.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
